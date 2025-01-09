44473584c8eea386a011a9aea73424e41fd6a853
BrillaldoSe <brillaldoarserra@gmail.com>
2025-01-09 13:57:23 -0600

    Avatar

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,27 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, useTexture } from "@react-three/drei";
+import { Avatar } from "./Avatar";
+import { useThree } from "@react-three/fiber";
 
 export const Experience = () => {
+  const texture = useTexture("textures/tuback.jpg");
+  const viewport = useThree((state) => state.viewport);
+
   return (
     <>
+      {/* Control de cámara */}
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
+
+      {/* Personaje */}
+      <Avatar position={[0, -3, 5]} scale={2}  />
+
+      {/* Fondo */}
+      <mesh >
+        <planeGeometry args={[viewport.width, viewport.height]} />
+        <meshBasicMaterial map={texture} />
       </mesh>
+
+      {/* Entorno */}
+      <Environment preset="sunset" />
     </>
   );
 };
